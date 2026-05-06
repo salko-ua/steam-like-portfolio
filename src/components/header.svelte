f02f9cb3709758e7d187b8ee1bc3e7e9ab811ad4
salko-ua <github@salko-ua.de>
2026-05-06 18:04:58 +0300

    add language page, change pfp and its shape

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -14,18 +14,17 @@
   <!-- avatar -->
   <div
     class="
-      min-h-42
-      min-w-42
-      h-42
-      w-42
-      max-xs:min-h-24
-      max-xs:min-w-24
-      max-xs:h-24
-      max-xs:w-24
-      mr-4
-    "
+    min-h-42 min-w-42 h-42 w-42
+    max-xs:min-h-24 max-xs:min-w-24 max-xs:h-24 max-xs:w-24
+    mr-4
+    [clip-path:polygon(0%_15%,_10%_5%,_20%_12%,_30%_3%,_40%_15%,_50%_4%,_60%_12%,_70%_2%,_85%_14%,_100%_8%,_100%_85%,_90%_95%,_80%_88%,_70%_98%,_60%_85%,_50%_95%,_40%_88%,_30%_100%,_15%_85%,_0%_92%)]
+  "
   >
-    <img alt="portrayal" src={asset("/pfp.jpg")} class="size-full" />
+    <img
+      alt="portrayal"
+      src={asset("/pfp.jpg")}
+      class="size-full object-cover"
+    />
   </div>
   <!-- name -->
   <div class="flex flex-col lg:pl-4 lg:pt-2">
---
+++ src/routes/languages/+page.svelte
@@ -0,0 +1,10 @@
+<p
+  class="sticky top-0 bg-gray-900/90 backdrop-blur-sm p-4 w-full text-center text-white text-xl"
+>
+  Ukrainian language
+</p>
+<p
+  class="sticky top-0 bg-gray-900/90 backdrop-blur-sm p-4 w-full text-center text-white text-xl"
+>
+  Ukrainian language
+</p>
